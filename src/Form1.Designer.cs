9d079abdfec88bbf1af564dad13281c63ee68fec
vdmkrchkn <vdmkrchkn@gmail.com>
2014-04-18 22:25:13 +0400

    Импорт исходников и изображений.

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace AI_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.new1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.open1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAs1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exit1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.image1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.segregate1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.image1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(223, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.new1,
+            this.open1,
+            this.saveAs1,
+            this.toolStripSeparator1,
+            this.exit1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // new1
+            // 
+            this.new1.Name = "new1";
+            this.new1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.new1.Size = new System.Drawing.Size(152, 22);
+            this.new1.Text = "&New";
+            this.new1.Click += new System.EventHandler(this.new1_Click);
+            // 
+            // open1
+            // 
+            this.open1.Name = "open1";
+            this.open1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.open1.Size = new System.Drawing.Size(152, 22);
+            this.open1.Text = "&Open";
+            this.open1.Click += new System.EventHandler(this.open1_Click);
+            // 
+            // saveAs1
+            // 
+            this.saveAs1.Name = "saveAs1";
+            this.saveAs1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveAs1.Size = new System.Drawing.Size(152, 22);
+            this.saveAs1.Text = "&Save as";
+            this.saveAs1.Click += new System.EventHandler(this.saveAs1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exit1
+            // 
+            this.exit1.Name = "exit1";
+            this.exit1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exit1.Size = new System.Drawing.Size(152, 22);
+            this.exit1.Text = "&Exit";
+            this.exit1.Click += new System.EventHandler(this.exit1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "bmp";
+            this.openFileDialog1.Filter = "BMP-files (*.bmp) | *.bmp";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "bmp";
+            this.saveFileDialog1.Filter = "BMP-files (*.bmp) | *.bmp";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(106, 104);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(113, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(98, 65);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Направление";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Down";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Up";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // image1
+            // 
+            this.image1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.segregate1});
+            this.image1.Name = "image1";
+            this.image1.Size = new System.Drawing.Size(52, 20);
+            this.image1.Text = "&Image";
+            // 
+            // segregate1
+            // 
+            this.segregate1.Name = "segregate1";
+            this.segregate1.Size = new System.Drawing.Size(152, 22);
+            this.segregate1.Text = "Se&gregate";
+            this.segregate1.Click += new System.EventHandler(this.segregate1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 128);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image generator";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem open1;
+        private System.Windows.Forms.ToolStripMenuItem saveAs1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exit1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem new1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ToolStripMenuItem image1;
+        private System.Windows.Forms.ToolStripMenuItem segregate1;
+
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AI_1
+{
+    public partial class Form1 : Form
+    {
+        private Form2 form2 = new Form2();
+        private Pen pen = Pens.Black;
+        private Point pt1, pt2, pt3;        
+        private const int shift = 1;
+        private Sample smpX, smpY;        
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddOwnedForm(form2);           
+            openFileDialog1.InitialDirectory = saveFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();            
+        }
+
+        private void exit1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void saveAs1_Click(object sender, EventArgs e)
+        {
+            string s0 = saveFileDialog1.FileName;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string s = saveFileDialog1.FileName;
+                if (s0.ToUpper().Equals(s.ToUpper()))
+                {
+                    s0 = Path.GetDirectoryName(s0) + "\\($$##$$).bmp";
+                    pictureBox1.Image.Save(s0);
+                    pictureBox1.Image.Dispose();
+                    File.Delete(s);
+                    File.Move(s0, s);
+                    pictureBox1.Image = new Bitmap(s);
+                }
+                else                
+                    pictureBox1.Image.Save(s);
+                //
+                if (radioButton1.Checked)
+                {
+                    smpX.Fill(pictureBox1.Image as Bitmap);
+                    smpX.Save("UpTriangle.txt");
+                }
+                else
+                {
+                    smpY.Fill(pictureBox1.Image as Bitmap);
+                    smpY.Save("DownTriangle.txt");
+                }
+            }
+        }
+
+        private void open1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string s = openFileDialog1.FileName;
+                try
+                {
+                    Image im = new Bitmap(s);
+                    if (pictureBox1.Image != null)
+                        pictureBox1.Image.Dispose();
+                    pictureBox1.Image = im;
+                }
+                catch
+                {
+                    MessageBox.Show("File " + s + " has a wrong format", "Error");
+                    return;
+                }
+                saveFileDialog1.FileName = s;
+                openFileDialog1.FileName = "";
+            }
+        }       
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {            
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (pt1.Y <= 0 || pt2.Y <= 0 || pt3.Y <= 0)
+                        return;
+                    pt1.Y -= shift;
+                    pt2.Y -= shift;
+                    pt3.Y -= shift;
+            	    break;
+                case Keys.Down:
+                    double dh = Math.Floor(pictureBox1.Image.Height * .95);
+                    if (pt1.Y >= dh || pt2.Y >= dh || pt3.Y >= dh)
+                        return;
+                    pt1.Y += shift;
+                    pt2.Y += shift;
+                    pt3.Y += shift;
+            	    break;
+                case Keys.Left:
+                    if(pt1.X <= 0)
+                        return;
+                    pt1.X -= shift;
+                    pt2.X -= shift;
+                    pt3.X -= shift;
+            	    break;
+                case Keys.Right:
+                    if (pt2.X >= pictureBox1.Image.Width - 1)
+                        return;
+                    pt1.X += shift;
+                    pt2.X += shift;
+                    pt3.X += shift;
+            	    break;
+                default:
+                    return;
+            }
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            g.DrawPolygon(pen, new Point[] { pt1, pt2, pt3 });
+            g.FillPolygon(new SolidBrush(pen.Color), new Point[] { pt1, pt2, pt3 });
+            pictureBox1.Invalidate();
+        }
+
+        private void new1_Click(object sender, EventArgs e)
+        {
+            form2.ActiveControl = form2.numericUpDown1;
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                saveFileDialog1.FileName = "";
+                int w = (int)form2.numericUpDown1.Value;
+                int h = (int)form2.numericUpDown2.Value;
+                int centerMargin = Math.Min(w, h) / 4;
+                Image im = new Bitmap(w, h);
+                Graphics g = Graphics.FromImage(im);                
+                if (radioButton1.Checked)
+                {
+                    smpX = new Sample(h, w);
+                    pt1 = new Point(w / 2 - centerMargin, h / 2 + centerMargin);
+                    pt2 = new Point(w / 2 + centerMargin, h / 2 + centerMargin);
+                    pt3 = new Point(w / 2, h / 2 - centerMargin);
+                }
+                else
+                {
+                    smpY = new Sample(h, w);
+                    pt1 = new Point(w / 2 - centerMargin, h / 2 - centerMargin);
+                    pt2 = new Point(w / 2 + centerMargin, h / 2 - centerMargin);
+                    pt3 = new Point(w / 2, h / 2 + centerMargin);
+                }                
+                g.Clear(pictureBox1.BackColor);
+                g.DrawPolygon(pen, new Point[] { pt1, pt2, pt3 });
+                g.FillPolygon(new SolidBrush(pen.Color), new Point[] { pt1, pt2, pt3 });
+                g.Dispose();
+                if (pictureBox1.Image != null)
+                    pictureBox1.Image.Dispose();
+                pictureBox1.Image = im;
+                this.ActiveControl = this.pictureBox1;                
+            }
+        }
+        /// <summary>
+        /// создание обучающих выборок
+        /// </summary>
+        /// <param name="cnt">число элементов выборки</param>
+        /// <returns>список, состоящий из 2 выборок разных классов изображений</returns>
+        List<Sample> InitW(int cnt)
+        {
+            List<Sample> sampling = new List<Sample>();            
+            for (int i = 1; i <= 2 * cnt; ++i)
+            {                
+                string s = "triangle" + i + "." + saveFileDialog1.DefaultExt;
+                Bitmap bmp;
+                try
+                {
+                    bmp = new Bitmap(@"..\\..\\images\\" + s);                   
+                }
+                catch
+                {
+                    MessageBox.Show("File " + s + " has a wrong format", "Error");
+                    return null;
+                }               
+                Sample elem = new Sample(bmp.Height + 1, bmp.Width, true);
+                elem.FillExtend(bmp, 1);
+                if (i > cnt)
+                    elem.Neg();                    
+                sampling.Add(elem);                
+            }
+            return sampling;
+        }        
+
+        private void segregate1_Click(object sender, EventArgs e)
+        {
+            // шаг 1 - инициализация
+            List<Sample> sampling = new List<Sample>(InitW(5));
+            if (sampling == null)
+                return;
+            int m = (int)form2.numericUpDown1.Value;
+            int n = (int)form2.numericUpDown2.Value;
+            Sample l = new Sample(m + 1, n, true);
+            l.FillExtend(1, 1);
+            int h = 10;        // скорость обучения
+            int iters = 10;    // кол-во итераций                        
+            // шаг 2 - итеративный процесс
+            for (int p = 0; p <= iters; ++p)                        
+            {
+                int min = int.MaxValue;
+                int idx = -1;
+                for (int i = 0; i < sampling.Count; ++i)
+                {
+                    int sc = l.scalarProduct(sampling[i]);
+                    if (sc < min)
+                    {
+                        min = sc;
+                        idx = i;
+                    }
+                }
+                if (min < 0)
+                {
+                    l = l.Sum(sampling[idx].mulScalar(h));
+                }
+                else if (min > 0)
+                {
+                    MessageBox.Show("Классы линейно разделимы");                    
+                    return;
+                }                
+            }
+            // шаг 3 - конец процесса
+            MessageBox.Show("Классы линейно неразделимы");
+        }
+
+        List<Sample> toConvexHull(List<Sample> ls)
+        {
+            List<Sample> w = new List<Sample>();
+            int smpCnt = ls.Count / 2;
+            for (int i = 0; i < smpCnt; ++i)
+                for (int j = smpCnt; j < ls.Count; ++j)
+                    w.Add(ls[i].Sum(ls[j]));
+            return w;
+        }
+
+        void Kozinets(List<Sample> w)
+        {
+            // шаг 1 - инициализация
+            double minNorma = double.MaxValue;
+            int idx = -1;
+            for (int i = 0; i < w.Count; ++i)
+                if (w[i].Norma() < minNorma)
+                {
+                    minNorma = w[i].Norma();
+                    idx = i;
+                }
+            if (idx < 0)
+                return;
+            Sample l0 = w[idx];
+            //  шаг 2 - итеративный процесс
+
+        }
+    }
+}
