e511f2758d33575175953bc17174bdc5875f476b
vdmkrchkn <vdmkrchkn@gmail.com>
2014-06-17 00:42:20 +0400

    класс программы

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 namespace AI_1
 {
     static class Program
-    {
+    {        
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
